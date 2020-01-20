f8609c9f8bfd40481cf0cec36e053b13e3de768e
Sean Doyle <sdoylelambda@gmail.com>
2020-01-20 11:52:05 -0500

    Initial commit.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,9 +1,13 @@
-# TO-DO: Complete the selection_sort() function below 
+# TO-DO: Complete the selection_sort() function below
+
+arr = [5, 4, 3, 1, 7, 2, 9]
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
+        print(smallest_index)
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
              
